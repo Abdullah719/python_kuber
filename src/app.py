8407590fd8b76a731e51a3ab8fbe1468f5954679
Abdullah Memon <abdullah.memon@newhaul.com.au>
2025-08-19 09:28:06 +0800

    Add python-app application with k8s deployment including docker,helm,argod

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify
+import  datetime
+import socket
+app = Flask(__name__)
+
+@app.route('/api/vq/details')
+
+def details():
+    return jsonify({
+        'time' : datetime.datetime.now().strftime("%H:%M:%S"),
+        'date': datetime.datetime.now().strftime("%Y-%m-%d"),
+        'host': socket.gethostname()
+    })
+
+@app.route('/api/vq/health')
+
+def health():
+    return jsonify({
+        "status": "up!"
+    }),200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
+
+
+#'/api/vq/details'
+#'/api/vq/health'
